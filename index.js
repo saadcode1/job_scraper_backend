b1f8712336f200ecad8d0af42628713a754401d3
Saad nizami <163667486+saadcode1@users.noreply.github.com>
2025-01-24 18:41:34 +0530

    Update index.js, corse error removed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const cors = require('cors');
+
 app.use(cors({ origin: '*', credentials: true }));
 
 
